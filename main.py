139e60774e9e2d9a6d1f60f8f234e63d076d7a27
rubyej27 <ruby_jackson1@my.vcccd.edu>
2024-07-02 11:16:42 -0400

    commit

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
 def isPrime(num):
-    ##################################################
-    # make your code
-    ##################################################
-    return 0
+   if num <= 1:
+       return False
+   if num == 2:
+       return True
+   if num == 3:
+       return True
+   if num % 2 == 0 or num % 3 == 0:
+       return False
+   i = 5
+   while i * i <= num:
+       if num % i == 0 or num % (i + 2) == 0:
+           return False
+       i += 6
+   return True
+
+
 
 
 def primeNumbers(begin, end):
-    ##################################################
-    # make your code
-    ##################################################
-    # isPrime(value)
+   rlist = []
+   for num in range(begin, end):
+       if isPrime(num):
+           rlist.append(num)
 
-    return plist
+
+   return rlist
+
+
 
 
 def main():
-    begin = 0
-    end = 20
-    rlst = primeNumbers(begin, end)
-    print(rlst)
+   begin = 0
+   end = 20
+   rlst = primeNumbers(begin, end)
+   print(rlst)
 
-    begin = 10
-    end = 50
-    rlst = primeNumbers(begin, end)
-    print(rlst)
+
+   begin = 10
+   end = 50
+   rlst = primeNumbers(begin, end)
+   print(rlst)
+
+
 
 
 if __name__ == '__main__':
-    main()
+   main()
